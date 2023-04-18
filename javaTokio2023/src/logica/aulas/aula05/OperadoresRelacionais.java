920056740e270a7982ccc4ebf9a3a583fffd356d
diegodiaaz <diego.reis.ruiz@gmail.com>
2023-04-18 18:37:40 -0300

    vendo operadores realcionais (comparacao)

---
+++ javaTokio2023/src/logica/aulas/aula05/OperadoresRelacionais.java
@@ -0,0 +1,24 @@
+package logica.aulas.aula05;
+
+public class OperadoresRelacionais {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(6 < 4);
+		
+		int num1 = 40;
+		System.out.println(num1 != 14);
+		System.out.println(num1 == 14);
+		System.out.println(false);
+		
+		boolean ligado = true;
+
+		System.out.println(" ");
+		
+		int num2 = 31;
+		System.out.println(num1 < num2);
+		
+		boolean comparacao = num1 <  num2;
+		System.out.println(comparacao);
+	}
+}
