26dbb860d19ad49a686275ff02252f64969c390f
diegodiaaz <diego.reis.ruiz@gmail.com>
2023-04-20 15:33:12 -0300

    exercicio finalizado, utilizando JOptionPane

--- javaTokio2023/src/logica/exercicios/aula05/Exercicio10.java
+++ javaTokio2023/src/logica/exercicios/aula05/Exercicio10.java
@@ -1,9 +1,17 @@
 package logica.exercicios.aula05;
+import javax.swing.*;
 
 public class Exercicio10 {
 
 	public static void main(String[] args) {
 
+		int idadeAnos = Integer.parseInt(JOptionPane.showInputDialog(null, "Quantos anos você tem?  ")); 
+		int idadeMeses = Integer.parseInt(JOptionPane.showInputDialog(null, "Quantos meses de idade você tem? ")); 
+		int idadeDias = Integer.parseInt(JOptionPane.showInputDialog(null, "Qauntos dias de idade você tem? ")); 
+		
+		int contaDias = idadeAnos * 365 + idadeMeses * 30 + idadeDias;
+		
+		JOptionPane.showMessageDialog(null, "Sua idade em dias é de: " + contaDias);
 		
 
 	}
