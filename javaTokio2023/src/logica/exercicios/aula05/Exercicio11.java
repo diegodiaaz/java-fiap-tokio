0b6cfbc6e7568048da9bcee234847b864a5ca01c
Diego Henrique <diego.reis.ruiz@gmail.com>
2023-04-21 03:22:13 -0300

    Desafio aula 05 resolvido

--- javaTokio2023/src/logica/exercicios/aula05/Exercicio11.java
+++ javaTokio2023/src/logica/exercicios/aula05/Exercicio11.java
@@ -5,12 +5,22 @@ public class Exercicio11 {
 
 	public static void main(String[] args) {
 
-		int idadeDias = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual a sua idade em dias?  ")); 
+		double idadeDias = Double.parseDouble(JOptionPane.showInputDialog(null, "Qual a sua idade em dias?  ")); 
 		
-		int contaAnos = idadeDias / 365;
+		double idadeAnos = idadeDias / 365;
+		double idadeMeses = (((idadeAnos - (int)idadeAnos) * 365) / 30);
+		double dias = ((idadeMeses - (int)idadeMeses) * 30);
 		
-		JOptionPane.showMessageDialog(null, "Sua idade é : " + contaAnos + " anos de idade!");
+		//JOptionPane.con(showMessageDialog(null, "A sua idade é: %.f", idadeAnos , " anos, %.0f", idadeMeses, " meses e %.0f", dias, "dias de idade!" ));
 		
+//		System.out.printf("\nA sua idade é: %.1f", idadeAnos);
+//		System.out.printf("\n meses: %.1f", idadeMeses);
+//		System.out.printf("\n e dias: %.1f", dias);
+		
+		
+		System.out.println("A sua idade é: " + (int)idadeAnos);
+		System.out.println("meses: " + (int)idadeMeses);
+		System.out.printf("e dias: %.0f", dias);
 
 	}
 
