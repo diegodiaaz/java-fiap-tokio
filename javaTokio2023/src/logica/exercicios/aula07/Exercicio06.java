817b50c561ba75b591a145ddcd86b9c615c06e96
Diego Henrique <diego.reis.ruiz@gmail.com>
2023-04-26 23:51:58 -0300

    tentando fazer ex 7

--- javaTokio2023/src/logica/exercicios/aula07/Exercicio06.java
+++ javaTokio2023/src/logica/exercicios/aula07/Exercicio06.java
@@ -1,11 +1,22 @@
 package logica.exercicios.aula07;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio06 {
 
 	public static void main(String[] args) {
 
+		int num = Integer.parseInt(JOptionPane.showInputDialog(null, "Insira o valor desejado: ")); 
 		
+		if (num < 2) {
+			JOptionPane.showMessageDialog(null, "ERRO, INSIRA MAIOR OU IGUAL A 2");
+		}
 
+		for (int i = 2; i <= num; i += 2) {
+			System.out.println(i);
+		}
+		
+		
 	}
 
 }
---
+++ javaTokio2023/src/logica/exercicios/aula07/Exercicio07.java
@@ -0,0 +1,28 @@
+//package logica.exercicios.aula07;
+//
+//import javax.swing.JOptionPane;
+//
+//public class Exercicio07 {
+//
+//	public static void main(String[] args) {
+//
+//		double[] nota = new double[2];
+//		double soma;
+//		double media = 6.0;
+//		int i = 0;
+//		int j = 0;
+//
+//		for (i = 0; i < nota.length; i++) {
+//
+//			nota[i] = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira a nota do " + (i + 1) + " aluno" + ["+nota+"]));
+//
+//			
+//			System.out.println(nota);
+//			if (nota > media) {
+//				j++;
+//			}
+//		}
+//
+//		JOptionPane.showMessageDialog(null, "O número de alunos com nota acima da média é: " + j);
+//	}
+//}
