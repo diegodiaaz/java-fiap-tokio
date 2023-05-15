c69fa43c8ef81af2d707edf680add3dd35199bad
Diego Henrique <diego.reis.ruiz@gmail.com>
2023-05-15 08:48:56 -0300

    Começando exercícios aula 09

--- javaTokio2023/src/logica/exercicios/aula08/Exercicio06.java
+++ javaTokio2023/src/logica/exercicios/aula08/Exercicio06.java
@@ -3,29 +3,10 @@ package logica.exercicios.aula08;
 public class Exercicio06 {
 
 	public static void main(String[] args) {
-
-//		int min = 0;
-//		int max = 101;
-//		int numSorteado = 0;
-//
-//		int qtdPrimos = 0;
-//
-//		for (int i = 0; i < 10; i++) {
-//			numSorteado = 5;
-//			System.out.println(numSorteado);
-//
-//		}
-//		for (int j = 2; j < numSorteado; j++) {
-//			int divisor = 2;
-//			while (numSorteado % divisor == 0) {
-//				divisor++;
-//			}
-//
-//			if (divisor == numSorteado) {
-//				qtdPrimos++;
-//			}
-//		}
-
+		
+		int max = 101;
+		
+		
+		
 	}
-
 }
---
+++ javaTokio2023/src/logica/exercicios/aula09/Exercicio01.java
@@ -0,0 +1,23 @@
+package logica.exercicios.aula09;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+
+//		Considerando o trecho de código Java representado a seguir:
+//
+		int[] v = {2, 0, 3, 9};
+		v[v[2]] = v[v[1]];
+		for (int i : v) {
+			System.out.println(i);
+		}
+		
+//			▪ O que será impresso na tela?
+//			a) 2 0 3 2
+//			b) 2 0 3 9
+//			c) 2 0 0 9
+//			d) 2 3 3 2
+
+	}
+
+}
